76b1811202ba0e1600482f013080f0c974be7817
Juan Pablo <juanpabloguirado@gmail.com>
2021-04-04 10:48:06 -0300

    first commit, entities and tables created

---
+++ src/main/java/com/JuanP/spingboot/RegistrationAndLoginAppWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.JuanP.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLoginAppWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAndLoginAppWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JuanP/spingboot/RegistrationAndLoginAppWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuanP.spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginAppWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
